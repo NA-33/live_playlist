d35892a4a3e06f0d2e93848037143190d0f72f7b
NA-33 <na33ttv@gmail.com>
2026-09-10 20:08:29 +0330

    main

--- live-playlist-frontend/.next/dev/static/chunks/live-playlist-frontend_219uq1s._.js
+++
@@ -1,5 +0,0 @@
-(globalThis["TURBOPACK_CHUNK_LISTS"] || (globalThis["TURBOPACK_CHUNK_LISTS"] = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: ["static/chunks/node_modules_next_dist_20wefz_._.js","static/chunks/node_modules_15xinfc._.js","static/chunks/live-playlist-frontend_src_app_page_tsx_0i9vy1i._.js"],
-    source: "entry"
-});
